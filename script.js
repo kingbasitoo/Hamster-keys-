6e47acf7d96efbd0e03a9718b20c8033f989188d
Basit Khan <123544842+kingbasitoo@users.noreply.github.com>
2024-08-23 21:03:00 +0500

    script.js

--- script.js
+++ script.js
@@ -103,7 +103,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
         attemptsNumber: 16,
 
-         }
+    },
+        8: {
+        'name': 'Mow and Trim',
+        'appToken': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        'promoId': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        'eventDelay': 20000
+        'attemptsNumber': 20,
+    },
+        9: {
+        'name': 'Mud Racing',
+        'appToken': '8814a785-97fb-4177-9193-ca4180ff9da8',
+        'promoId': '8814a785-97fb-4177-9193-ca4180ff9da8',
+        'eventDelay': 20000
+        'attemptsNumber': 20,
+            }
 
     };
 
